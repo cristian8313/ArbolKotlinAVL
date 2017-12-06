9430e33cde5a228fab901a4d876db70c45a1f3d8
cristian8313 <mm.dellepiane@gmail.com>
2017-12-06 16:15:12 -0300

    Update Nodo.kt
    recorrido va en arbol

--- src/Nodo.kt
+++ src/Nodo.kt
@@ -1,12 +1,4 @@
 data class Nodo (var izquierdo: Nodo?, var derecho: Nodo?, var padre: Nodo?, var balance: Int, var dato: Elemento) {
 
-    fun enOrden() {
-        if(this.izquierdo != null)
-            this.izquierdo?.enOrden();
-
-        println("Elemento: " + this.dato.toString());
-
-        if(this.derecho != null)
-            this.derecho?.enOrden();
-    }
+    
 }
