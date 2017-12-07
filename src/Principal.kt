3f3ec341b97d49f78fdd5f8018bb8ee361f398d3
cristiandellepiane <mm.dellepiane@gmail.com>
2017-12-06 21:31:01 -0300

    metodo compareTo de elementos

--- src/Principal.kt
+++ src/Principal.kt
@@ -1,44 +1,16 @@
 
 fun main(args: Array<String>) {
-    /*var e1 = Elemento('a',1)
-    var e2 = Elemento('a',1)
-    var e3 = Elemento('c',1)
-    var e4 = Elemento('a',1)
 
-    var a = Arbol(null)
-
-    a.raiz = a.insertarNodo(a.raiz, e1)
-    a.raiz = a.insertarNodo(a.raiz, e2)
-    a.raiz = a.insertarNodo(a.raiz, e3)
-    a.raiz = a.insertarNodo(a.raiz, e4)
-
-    //a.imprimir(a.raiz)
-
-
+    val arbol = Arbol(null)
 
     var str : String
-    str = "hola como estas"
 
-    var a2 = Arbol(Nodo(null,null, Elemento('h',1)))
+    str = "bbbbeeeeeeetthh"
 
     for (c in str) {
-        //println(c)
-        a2.raiz = a2.insertarNodo(a2.raiz, Elemento(c,1))
+        arbol.insertar(Elemento(c,1))
     }
 
-    a2.imprimir(a2.raiz)
-    println("Letra: " + a2.raiz?.dato?.letra + "  - Prioridad: " + a2.raiz?.dato?.prioridad)*/
-    val arbol = Arbol(null)
-    
-    var e1 = Elemento('a',1)
-    var e2 = Elemento('a',1)
-    var e3 = Elemento('c',1)
-    var e4 = Elemento('a',1)
-    
-    arbol.insert(e1)
-    arbol.insert(e2)
-    arbol.insert(e3)
-    arbol.insert(e4)
-    
-    arbol.imprimir(arbol.raiz)  
+    println(arbol.raiz?.dato.toString())
+    arbol.imprimir(arbol.raiz)
 }
